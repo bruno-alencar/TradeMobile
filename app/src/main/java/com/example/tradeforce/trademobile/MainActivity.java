b66af8674423e8d0a1d3bc278822291d6cfb729c
André Victor do Nascimento Leite <andrevnl@hotmail.com>
2016-09-30 23:50:08 -0300

    Sexta a noite

--- app/src/main/java/com/example/tradeforce/trademobile/MainActivity.java
+++ app/src/main/java/com/example/tradeforce/trademobile/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.tradeforce.trademobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textoMapa = (TextView) findViewById(R.id.txtviewMapa);
+        Button btnRotas = (Button) findViewById(R.id.btnRotas);
+
+        textoMapa.setText("Teste");
+        textoMapa.setTextSize(40);
+
+        btnRotas.setText("Clique aqui");
+
+        btnRotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textoMapa.setText("Botao apertado");
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
